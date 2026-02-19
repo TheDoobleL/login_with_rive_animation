baf4edd626501067ed7a39ba836c92d058e7a93e
TheDoobleL <luiscarrillo152@gmail.com>
2026-02-19 10:20:55 -0600

    feat(login): focus-based bear reactions

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,6 +19,29 @@ class _LoginScreenState extends State<LoginScreen> {
   SMITrigger? _trigSuccess;
   SMITrigger? _trigFail;
 
+  //1.1) Crear variables para FocusNode
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+
+  //1.2) Listener para FocusNode(Oyentes/chismosos)
+  @override
+  void initState() {
+    super.initState();
+    _emailFocusNode.addListener(() {
+      if (_emailFocusNode.hasFocus) {
+        //verifica que sea nulo
+        if (_isHandsUp != null) {
+          // manos abajo en el email
+          _isHandsUp!.change(false);
+        }
+      }
+    });
+    _passwordFocusNode.addListener(() {
+      //manos arriba en password
+      _isHandsUp!.change(_passwordFocusNode.hasFocus);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -57,10 +80,12 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 24),
               TextField(
+                //1.3 Asignar FocusNode al Textfield
+                focusNode: _emailFocusNode,
                 onChanged: (value) {
                   if (_isHandsUp != null) {
                     //No tapes los ojos al ver email
-                    _isHandsUp!.change(false);
+                    //_isHandsUp!.change(false);
                   
                   }
                   //Si isChecking no es nulo
@@ -112,5 +137,13 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+  }
+
+//1.4 Liberar memoria/recursos al salir de la pantalla
+@override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
   }
 }
